8caddbe96778cecb26e8532615bccb6526e0a0b7
mtkubota <mattkay5@gmail.com>
2015-11-03 12:57:29 -1000

    review

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,37 @@
 def division(num1, num2)
- 
+  (12 / 2)
 end
 
 def assign_variable(value)
+  value = "Bob"
   
 end
 
-def argue
-  
+def argue(phrase = "I'm right and you are wrong!")
+  puts "phrase"
+  phrase
 end
 
-def greeting
+def greeting(greeting, name)
+  puts "#{greeting}, #{name}"
 
 end
 
 def return_a_value
+  "Nice"
 
 end
 
 def last_evaluated_value
+  "expert"
 
 end
 
-def pizza_party
+def pizza_party(name = "cheese")
+  puts name
+  name
 
 end
+
+pizza_party
+pizza_party("pepperoni")
